84afa1f0b530fc96a1de25686fd3e72209f0144b
shirakigh <shiraki@plum.ocn.ne.jp>
2016-05-03 00:47:10 +0900

    Fullcalendarにデモ用イベント追加

--- webroot/js/wotasuke/full_calendar.js
+++ webroot/js/wotasuke/full_calendar.js
@@ -53,9 +53,56 @@ $(function () {
 		columnFormat: {
 			week: "M/D[(]ddd[)]",
   	},
+    //Random default events
+    events: [
+      {
+        title: 'イベント',
+        start: new Date(y, m, 1),
+        backgroundColor: "#f56954", //red
+        borderColor: "#f56954" //red
+      },
+      {
+        title: '旅行',
+        start: new Date(y, m, d - 5),
+        end: new Date(y, m, d - 2),
+        backgroundColor: "#f39c12", //yellow
+        borderColor: "#f39c12" //yellow
+      },
+      {
+        title: 'イベント',
+        start: new Date(y, m, d, 10, 30),
+        allDay: false,
+        backgroundColor: "#0073b7", //Blue
+        borderColor: "#0073b7" //Blue
+      },
+      {
+        title: 'ライブ',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false,
+        backgroundColor: "#00c0ef", //Info (aqua)
+        borderColor: "#00c0ef" //Info (aqua)
+      },
+      {
+        title: '生誕祭',
+        start: new Date(y, m, d + 1, 19, 0),
+        end: new Date(y, m, d + 1, 22, 30),
+        allDay: false,
+        backgroundColor: "#00a65a", //Success (green)
+        borderColor: "#00a65a" //Success (green)
+      },
+      {
+        title: 'ニコ生',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        url: 'http://google.com/',
+        backgroundColor: "#3c8dbc", //Primary (light-blue)
+        borderColor: "#3c8dbc" //Primary (light-blue)
+      }
+    ],
 
 		//events: "<?= $this->Url->build('/events/feed/', true) ?>",
-		events: '/ajax/feed',
+		//events: '/ajax/feed',
 		eventRender: function(event, element) {
 				element.qtip({
 						content: event.details,
